42c353bf71e36ccbc1245127724ca5614200f6ac
Iman Ghanei <ghaneiash@gmail.com>
2022-03-24 14:20:27 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
 }
 
 export default App;
+
+// 1. firestly we have to know HTTP request methods (GET, POST, PUT, DELETE, ...)(Google it)
+// 2. We can use Fetch (We can send our requests to server) (Google it)
+// 3. But there is a better way, and we can use axios library (Google it)
+// 4. Now how we can get our DATABASES? We can use a Fake Database like (JSON Placeholder(jsonplaceholder.typicode.com))
+// 5. Now install our axios -> npm i axios
