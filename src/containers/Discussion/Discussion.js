1e7a7bb8a9909933df9679f74c7dfeceffad3d9e
Iman Ghanei <ghaneiash@gmail.com>
2022-03-24 15:03:58 +0100

    changes

--- src/containers/Discussion/Discussion.js
+++ src/containers/Discussion/Discussion.js
@@ -1,12 +1,20 @@
 import Comment from "../../components/Comment/Comment";
+import FullComment from "../../components/FullComment/FullComment";
+import NewComment from "../../components/NewComment/NewComment";
 const Discussion = () => {
   return (
     <main>
-      <section>
+      <section className="post">
+        <Comment />
+        <Comment />
         <Comment />
       </section>
-      <section>full comment</section>
-      <section>add new comment</section>
+      <section>
+        <FullComment />
+      </section>
+      <section>
+        <NewComment />
+      </section>
     </main>
   );
 };
