522877608c24fbdf6b1452e161782e3d082553a6
Iman Ghanei <ghaneiash@gmail.com>
2022-03-28 11:39:01 +0200

    changes

--- src/containers/Discussion/Discussion.js
+++ src/containers/Discussion/Discussion.js
@@ -5,20 +5,23 @@ import NewComment from "../../components/NewComment/NewComment";
 import axios from "axios";
 import "./discussion.css";
 const Discussion = () => {
-  // HOW TO GET DATA FROM BACKEND?
-  // 1. useEffect () => http
-  // 2. CDM => get (with first render we will get data from backend)
-
-  // Now let's get data from backend
-  // useEffect has a callback function and a dependency
+  // If status was :
+  // in range of 200 -> everything ok
+  // 301, 302 -> redirect
+  // 401 -> unAuthorized / 402, 403 -> noe access / 404 -> not found
+  // in range of 500 -> is in backend and server, that'S not my problem
   useEffect(() => {
     // SO when we use get (for promise), after that we use these 2 methods -> then or catch
     // then () is when our promise has not any problem
     // catch () is when our catch has problem
     axios
       .get("https://jsonplaceholder.typicode.com/comments")
-      .then(() => {})
-      .catch(() => {});
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
   return (
     <main>
