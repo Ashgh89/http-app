43725eca74c8de3628068451d79dd9d5f0ec6d51
Iman Ghanei <ghaneiash@gmail.com>
2022-03-28 11:16:13 +0200

    changes

--- src/containers/Discussion/Discussion.js
+++ src/containers/Discussion/Discussion.js
@@ -2,14 +2,19 @@ import { useEffect } from "react";
 import Comment from "../../components/Comment/Comment";
 import FullComment from "../../components/FullComment/FullComment";
 import NewComment from "../../components/NewComment/NewComment";
+import axios from "axios";
 import "./discussion.css";
 const Discussion = () => {
   // HOW TO GET DATA FROM BACKEND?
   // 1. useEffect () => http
   // 2. CDM => get (with first render we will get data from backend)
 
+  // Now let's get data from backend
   // useEffect has a callback function and a dependency
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const promis = axios.get("https://jsonplaceholder.typicode.com/comments");
+    console.log(promis);
+  }, []);
   return (
     <main>
       <section>
