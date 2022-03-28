46f0723eb537ad921aaed1eccec3046c8394324a
Iman Ghanei <ghaneiash@gmail.com>
2022-03-28 11:09:22 +0200

    changes

--- src/containers/Discussion/Discussion.js
+++ src/containers/Discussion/Discussion.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Comment from "../../components/Comment/Comment";
 import FullComment from "../../components/FullComment/FullComment";
 import NewComment from "../../components/NewComment/NewComment";
 import "./discussion.css";
 const Discussion = () => {
+  // HOW TO GET DATA FROM BACKEND?
+  // 1. useEffect () => http
+  // 2. CDM => get (with first render we will get data from backend)
+
+  // useEffect has a callback function and a dependency
+  useEffect(() => {}, []);
   return (
     <main>
       <section>
