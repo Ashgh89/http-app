cc6a49fb95dcc374be7b67df1ec1bdf34e3b3e5b
Iman Ghanei <ghaneiash@gmail.com>
2022-03-29 14:41:42 +0200

    changes

--- src/components/FullComment/FullComment.js
+++ src/components/FullComment/FullComment.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import "./fullComment.css";
+import axios from "axios";
 const FullComment = ({ commentId }) => {
-  console.log(commentId);
+  const [comment, setComment] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+      .then((res) => setComment(res.data))
+      .catch();
+  }, [commentId]);
+  const styles = {
+    color: "#444",
+    backgroundColor: "#efefef",
+    padding: "10px",
+  };
+  if (!commentId) return <p style={styles}>Please Select a Comment</p>;
   return (
     <div className="fullComment">
-      <p>name</p>
-      <p>email</p>
-      <p>body</p>
+      <p>name: {comment.name}</p>
+      <p>email: {comment.email}</p>
+      <p>{comment.body}</p>
     </div>
   );
 };
