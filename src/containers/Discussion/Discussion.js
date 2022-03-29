b3aaa1cf50f4bb44da6e18875c2a2e36fa549224
Iman Ghanei <ghaneiash@gmail.com>
2022-03-29 14:11:56 +0200

    chnges

--- src/containers/Discussion/Discussion.js
+++ src/containers/Discussion/Discussion.js
@@ -10,39 +10,23 @@ const Discussion = () => {
   // Now let's get data from backend
   // useEffect has a callback function and a dependency
   useEffect(() => {
-    // SO when we use get (for promise), after that we use these 2 methods -> then or catch
-    // then () is when our promise has not any problem
-    // catch () is when our catch has problem
-    // axios
-    //   .get("https://jsonplaceholder.typicode.com/comments")
-    //   .then((response) => {
-    //     // console.log(response);
-    //     setComments(response.data.slice(0, 4));
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    const getComment = async () => {
+      try {
+        // With Object destructuring ({data})
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        setComments(data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getComment();
   }, []);
 
   // THERE IS A ANOTHER WAY TO GET DATA FROM SERVER AND BACKEND
   // Async - Await
-  async function getComment() {
-    // Without Object destructuring ({data})
-    //  const myData = await axios.get(
-    //   "https://jsonplaceholder.typicode.com/comments"
-    // );
-    // setComments(myData.data.slice(0, 4));
-    try {
-      // With Object destructuring ({data})
-      const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/comments"
-      );
-      setComments(data.slice(0, 4));
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
     <main>
