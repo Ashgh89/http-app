aeb966a1b5807e38c1b26cc69c91e29a455be4f3
Iman Ghanei <ghaneiash@gmail.com>
2022-03-30 16:02:27 +0200

    changes

--- src/components/FullComment/FullComment.js
+++ src/components/FullComment/FullComment.js
@@ -2,27 +2,35 @@ import { useEffect, useState } from "react";
 import "./fullComment.css";
 import axios from "axios";
 const FullComment = ({ commentId }) => {
-  const [comment, setComment] = useState(null);
-  useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
-      .then((res) => setComment(res.data))
-      .catch();
-  }, [commentId]);
+  if (!commentId) return <p>Please select a comment!</p>;
+  // const [comment, setComment] = useState(null);
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+  //     .then((res) => setComment(res.data))
+  //     .catch();
+  // }, [commentId]);
 
-  let commentDetail = <p>please select a comment!</p>;
-  if (commentId) commentDetail = <p>LOADING.....</p>;
+  // let commentDetail = <p>please select a comment!</p>;
+  // if (commentId) commentDetail = <p>LOADING.....</p>;
 
-  if (comment) {
-    commentDetail = (
-      <div className="fullComment">
-        <p> {comment.name}</p>
-        <p>{comment.email}</p>
-        <p>{comment.body}</p>
-      </div>
-    );
-  }
-  return commentDetail;
+  // if (comment) {
+  //   commentDetail = (
+  //     <div className="fullComment">
+  //       <p> {comment.name}</p>
+  //       <p>{comment.email}</p>
+  //       <p>{comment.body}</p>
+  //     </div>
+  //   );
+  // }
+  // return commentDetail;
+  return (
+    <div className="fullComment">
+      <p>name</p>
+      <p>email</p>
+      <p>body</p>
+    </div>
+  );
 };
 
 export default FullComment;
