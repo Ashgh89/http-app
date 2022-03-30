813a7de12c2c9d909e59534b9537b1b96a9a448b
Iman Ghanei <ghaneiash@gmail.com>
2022-03-30 17:00:11 +0200

    changes

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import "./newComment.css";
 
 const NewComment = () => {
+  const [comment, setComment] = useState({
+    name: "",
+    email: "",
+    content: "",
+  });
+  const nameHandler = (e) => {
+    setComment({ ...comment, name: e.target.value });
+  };
+  const emailHandler = (e) => {
+    setComment({ ...comment, email: e.target.value });
+  };
+  const bodyHandler = (e) => {
+    setComment({ ...comment, content: e.target.value });
+  };
   return (
     <div className="newComment">
       <div>
         <label>name</label>
-        <input type="text" />
+        <input type="text" onChange={nameHandler} />
       </div>
       <div>
         <label>email</label>
-        <input type="email" />
+        <input type="email" onChange={emailHandler} />
       </div>
       <div>
         <label>body</label>
-        <input type="textarea" />
+        <input type="textarea" onChange={bodyHandler} />
       </div>
     </div>
   );
