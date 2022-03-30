8c63571f03090927943460effedef1a507595c0d
Iman Ghanei <ghaneiash@gmail.com>
2022-03-30 17:40:48 +0200

    changes

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./newComment.css";
+import axios from "axios";
 
 const NewComment = () => {
   const [comment, setComment] = useState({
@@ -16,6 +17,12 @@ const NewComment = () => {
   const bodyHandler = (e) => {
     setComment({ ...comment, content: e.target.value });
   };
+  const postCommentHandler = () => {
+    // We want to send data to BACKEND
+    // name, email, content, postId=10
+    // create =>
+    axios.post("https://jsonplaceholder.typicode.com/comments/", comment);
+  };
   return (
     <div className="newComment">
       <div>
@@ -30,6 +37,7 @@ const NewComment = () => {
         <label>body</label>
         <input type="textarea" onChange={bodyHandler} />
       </div>
+      <button onClick={postCommentHandler}>Add New Comment</button>
     </div>
   );
 };
