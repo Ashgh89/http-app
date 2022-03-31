d678688b2728c3002da3d0950bcba7e371eaa5a6
Iman Ghanei <ghaneiash@gmail.com>
2022-03-31 20:10:44 +0200

    changes

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -2,41 +2,30 @@ import { useState } from "react";
 import "./newComment.css";
 import axios from "axios";
 
-const NewComment = ({ onAddPost }) => {
+const NewComment = ({ /*onAddPost*/ setComments }) => {
   const [comment, setComment] = useState({
     name: "",
     email: "",
     content: "",
   });
-  // const nameHandler = (e) => {
-  //   setComment({ ...comment, name: e.target.value });
-  // };
-  // const emailHandler = (e) => {
-  //   setComment({ ...comment, email: e.target.value });
-  // };
-  // const bodyHandler = (e) => {
-  //   setComment({ ...comment, content: e.target.value });
-  // };
 
   const changeHandler = (e) => {
     // To be sure if it works look in Console, Components and Network
     setComment({ ...comment, [e.target.name]: e.target.value });
   };
-  // const postCommentHandler = () => {
-  //   // We want to send data to BACKEND
-  //   // name, email, content, postId=10
-  //   // create =>
-  //   // NOTICE axios is not exactly like json, it says send an object and dont worry i'll do the rest (like stringify)
-  //   axios
-  //     // Now if we want to add to this data another thing (e.g postId)
-  //     .post("http://localhost:3001/comments/", {
-  //       ...comment,
-  //       postId: 10,
-  //     })
-  //     // NOTICE don't forget to give res.data for the last pic here
-  //     .then((res) => console.log(res.data))
-  //     .catch();
-  // };
+  const postCommentHandler = () => {
+    axios
+      // Now if we want to add to this data another thing (e.g postId)
+      .post("http://localhost:3001/comments/", {
+        ...comment,
+        postId: 10,
+      })
+      // NOTICE don't forget to give res.data for the last pic here
+      .then((res) => axios.get("http://localhost:3001/comments/"))
+      .then((res) => setComments(res.data))
+      .catch();
+  };
+
   return (
     <div className="newComment">
       <div>
@@ -51,7 +40,7 @@ const NewComment = ({ onAddPost }) => {
         <label>body</label>
         <input type="textarea" onChange={changeHandler} name="content" />
       </div>
-      <button onClick={() => onAddPost(comment)}>Add New Comment</button>
+      <button onClick={postCommentHandler}>Add New Comment</button>
     </div>
   );
 };
