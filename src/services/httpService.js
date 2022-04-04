ba092139c7e5b50310959fe16463705cc646d171
Iman Ghanei <ghaneiash@gmail.com>
2022-04-04 16:52:24 +0200

    chnages

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -8,6 +8,17 @@ axios.defaults.baseURL = "http://localhost:3001";
 // And we must delete export default axios and that's Aufwändig
 // export default axios;
 // So what to do?
+
+// Firstly (Write our baseURL in just one file)
+// We know about axios so far, but in real projects,
+// we have many function that are relevant to backend.
+// SO we try to write clean codes which are relevant to API and backend
+// So what if for example if our http://localhost:3001 change?
+// So we have to change all of them and that'S not good.
+// So we can write it here and if we want to change it, we can change just here easily.
+
+// Secondly (Error Handling)
+//
 const http = {
   get: axios.get,
   put: axios.put,
