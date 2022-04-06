69bc25d802bda97706b2e974a7a65dd0cef101b5
Iman Ghanei <ghaneiash@gmail.com>
2022-04-06 13:04:28 +0200

    changes

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,10 +1,12 @@
 import "./comment.css";
 
-const Comment = ({ name, email, onClick }) => (
-  <div className="comment" onClick={onClick}>
-    <p>name: {name}</p>
-    <p>email: {email}</p>
-  </div>
-);
+const Comment = ({ name, email, onClick }) => {
+  return (
+    <div className="comment" onClick={onClick}>
+      <p>name: {name}</p>
+      <p>email: {email}</p>
+    </div>
+  );
+};
 
 export default Comment;
