316394e33f4c2ad95cbd902e6ac33e6dd4135489
Iman Ghanei <ghaneiash@gmail.com>
2022-04-07 20:01:37 +0200

    changes

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -6,7 +6,7 @@ import { getAllComments } from "../../services/getAllCommentsService";
 
 const NewComment = ({ setComments }) => {
   const [comment, setComment] = useState({
-    name: "sasw",
+    name: "",
     email: "",
     content: "",
   });
